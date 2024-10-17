ed8969e8e6d0447555c0e42732af34a1db5b3e38
sukmin11 <sukmin1105@naver.com>
2024-10-17 21:43:40 +0900

    (2024.10.17) 생성자와 소멸자

---
+++ Class2th (Name/Source.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+
+using namespace std;
+
+namespace Korea
+{
+	float area;
+
+	void Information()
+	{
+		cout << "면적 : " << area << endl;
+	}
+}
+
+namespace Australia
+{
+	float area;
+
+	void Information()
+	{
+		cout << "면적 : " << area << endl;
+	}
+}
+
+using namespace Korea;
+
+class Object
+{
+#pragma region 접근 지정자
+	// 클래스 내부의 포함되어 있는 속성에 접근 범위를
+	// 제한하는 지정자입니다.
+
+	// public : 클래스 내부와 자기가 상속하고 있는 클래스 그리고 클래스
+	//          외부에서 접근을 허용하는 접근 지정자입니다.
+
+	// protected : 클래스 내부와 자기가 상속하고 있는 클래스에서만
+	//             접근을 허용하는 접근 지정자입니다.
+
+	// private : 클래스 내부에서만 접근을 허용하는 접근 지정자입니다.
+#pragma endregion
+
+public:
+	int x;
+
+	int X()
+	{
+		return x;
+	}
+
+protected:
+	int y;
+private:
+	int z;
+};
+
+int main()
+{
+#pragma region 이름 공간
+	// 속성을 구분할 수 있도록 유효 범위를 설정하는 영역입니다.
+
+	// area = 1000;
+	// Information();
+	// 
+	// Australia::area = 9999;
+	// Australia::Information();
+
+	// 이름 공간은 같은 이름의 함수나 변수를 또 다른 공간에 선언하여
+	// 중복되지 않도록 선언할 수 있습니다.
+
+#pragma endregion
+
+#pragma region 클래스
+	// 사용자 정의 데이터 유형으로 속성과 함수가 포함되어
+	// 있으며, 클래스를 통해 객체를 생성하여 접근하고 사용
+	// 할 수 있는 집합체입니다.
+
+	// Object object1;
+	// Object object2;
+	// Object object3;
+	// 
+	// object1.x = 5;
+	// object2.x = 25;
+	// object3.x = 10;
+	// 
+	// cout << "Object의 크기 : " << sizeof(Object) << endl;
+	// cout << "Object1.x : " << object1.X() << endl;
+	// cout << "Object2.x : " << object2.X() << endl;
+	// cout << "Object3.x : " << object3.X() << endl;
+
+	// 클래스의 경우 클래스 내부에 있는 변수는 클래스의
+	// 메모리 영역에 포함되지만, 정적 변수와 함수의 메모리는
+	// 클래스 영역에 포함되지 않습니다.
+#pragma endregion
+
+
+	return 0;
+}
+
---
+++ Class3th (Constructor & Destructor)/Source.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class GameObject
+{
+private:
+	static int count;
+	const char* name;
+
+public:
+	GameObject()
+	{
+		name = "Cylinder";
+
+		cout << "Create GameObject" << endl;
+	}
+
+	void Information() const
+	{
+		// name = "Box";
+
+		cout << "name : " << name << endl;
+		cout << "Count의 값 : " << count << endl;
+	}
+
+#pragma region 소멸자
+	// 객체가 소멸될 때 자동으로 실행되는 클래스의
+	// 멤버 함수입니다.
+
+	~GameObject()
+	{
+		count++;
+
+		cout << "Destroy GameObject" << endl;
+	}
+	// 소멸자는 객체가 메모리에서 해제될 때 단 한 번
+	// 만 호출되며, 소멸자에는 매개 변수를 생성하여
+	// 사용할 수 없습니다.
+#pragma endregion
+
+};
+
+int GameObject::count = 0;
+
+void OnDamage(int& stamina)
+{
+	stamina -= 10;
+}
+
+int main()
+{
+#pragma region 생성자
+	// 클래스의 인스턴스가 생성되는 시점에서 자동으로 
+	// 호출되는 특수한 멤버 함수입니다.
+
+	// GameObject gameObject;
+	// 
+	// GameObject* gameObject1 = new GameObject();
+	// GameObject* gameObject2 = new GameObject();
+	// GameObject* gameObject3 = new GameObject();
+	// 
+	// delete gameObject1;
+	// delete gameObject2;
+	// delete gameObject3;
+	// 
+	// gameObject.Information();
+
+	// 생성자의 경우 객체가 생성될 때 단 한 번만 호출
+	// 되며, 생성자는 반환형이 존재하지 않기 때문에
+	// 생성자가 호출되기 전에는 객체에 대한 메모리는
+	// 할당되지 않습니다.
+
+#pragma endregion
+
+#pragma region 참조자
+	// 할당된 하나의 메모리 공간에 다른 이름을
+	// 붙여 사용하는 키워드입니다.
+
+	// int health = 100;
+	// 
+	// int& reference = health;
+	// 
+	// reference = 99;
+	// 
+	// OnDamage(health);
+	// 
+	// cout << "health 변수의 값 : " << health << endl;
+	// cout << "reference 변수의 값 : " << reference << endl;
+
+#pragma endregion
+
+
+	return 0;
+}
+
